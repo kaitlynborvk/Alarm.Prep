9394c04478c45912ab362e6cda57ce43d5553eef
byselonia <byselonia@gmail.com>
2025-06-19 01:11:48 -0400

    Add Stats tab to bottom navigation between Alarms and Settings

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -23,6 +23,17 @@ export default function BottomNav() {
             <span className="text-xs mt-1">Alarms</span>
           </Link>
           <Link 
+            href="/stats"
+            className={`flex flex-col items-center justify-center w-full h-full ${
+              pathname === '/stats' ? 'text-blue-600' : 'text-gray-600'
+            }`}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+            </svg>
+            <span className="text-xs mt-1">Stats</span>
+          </Link>
+          <Link 
             href="/settings"
             className={`flex flex-col items-center justify-center w-full h-full ${
               pathname === '/settings' ? 'text-blue-600' : 'text-gray-600'
